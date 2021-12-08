40a28821d8ecf90619dbe9c8daebe45a43c91e0a
Damian Noga <d.nogaa@gmail.com>
2021-12-09 00:29:43 +0100

    Naprawa kompilacji

---
+++ BusinessLogic/BusinessLogicModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+  public class BusinessLogicModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      base.Load(builder);
+      //TODO: To tu ;)
+    }
+  }
+}
---
+++ Repository/RepositoryModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Contract.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+  public class RepositoryModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      base.Load(builder);
+      //Todo: To tu ;)
+    }
+  }
+}
